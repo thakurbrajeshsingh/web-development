2cb7e6868de86963eb617cdd305631a7e3e2c981
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-01 23:32:28 +0530

    Contact List -NodeJS

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const port = 8000;
+
+
+
+
+const app=express();
+
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+
+app.use(function(res,req,next){
+    console.log("MiddleWare 1 Called");
+    next();
+});
+app.use(function(res,req,next){
+    console.log("MiddleWare 2 Called");
+    next();
+});
+
+
+
+
+
+
+var contactList = [
+    {
+        name:"Brajesh",
+        phone:"1234567890"
+    },
+    {
+        name:"Rahul",
+        phone:"865836588"
+    },
+    {
+        name:"Bhavya",
+        phone:"782785543"
+    }
+]
+
+app.get('/',(req,res)=>{
+    return res.render('home',{ 
+    title :'Contact List',
+    contact_list : contactList});
+})
+
+app.get('/practice',(req,res)=>{
+    return res.render('practice',{
+        title:"Player Count"
+    })
+})
+
+app.post('/create-contact',(req,res)=>{
+    // return res.redirect('/practice')
+    // contactList.push({
+    //     name : req.body.name,
+    //     phone : req.body.phone
+    // })
+    contactList.push(req.body);
+    // return res.redirect('/');
+    return res.redirect('/');
+})
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log("Yup! Error Occured!");
+    }
+    console.log("Server Running at Port : ",port);
+});
+
+
+
